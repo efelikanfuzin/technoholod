cfa7a2f2012c8cc1c9159bf0ecc4a273b877239c
Klimov Aleksandr <efelikanfuzin@gmail.com>
2016-02-06 13:36:05 +0300

    [add] providers block, fix some bugs

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var swiper = new Swiper('.swiper-container', {
+  Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
     paginationClickable: true,
     nextButton: '.reviews_next',
