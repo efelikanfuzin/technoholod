c12c7c9933b992d21b86153e2ed2253dc1634cdd
Oxana <pigarevaoks@gmail.com>
2016-05-29 21:54:51 +0000

    project show page

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
     prevButton: '.reviews_prev',
     loop: true,
   });
+  
+    Swiper($('[data-prodject-gal]'), {
+    paginationClickable: true,
+    loop: true,
+		slidesPerView: 4,
+    nextButton: '[data-prodject-gall-next]',
+		prevButton: '[data-prodject-gall-prev]',
+    loop: true,
+  });
+  
 });
