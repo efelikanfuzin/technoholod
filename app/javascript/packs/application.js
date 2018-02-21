6d1617e29b8c84f9b1e2605c0ac9e0975f560c2f
Alex Klimov <efelikanfuzin@gmail.com>
2018-02-21 12:04:50 +0300

    Use stylesheets by webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,28 @@
 import '../styles/application.scss';
+import Swiper from 'swiper'
+
+new Swiper('[data-main-gal]', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.reviews_next',
+    prevButton: '.reviews_prev',
+    autoplay: true,
+    loop: true
+});
+
+new Swiper(document.querySelector('[data-reviews-gal]'), {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.reviews_next',
+    prevButton: '.reviews_prev',
+    loop: true,
+});
+
+new Swiper(document.querySelector('[data-prodject-gal]'), {
+    paginationClickable: true,
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    nextButton: '[data-prodject-gall-next]',
+    prevButton: '[data-prodject-gall-prev]',
+});
