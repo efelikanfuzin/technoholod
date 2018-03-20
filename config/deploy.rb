a3c2cbff1768414620eded957d3044c10bcd88e0
Alex Klimov <efelikanfuzin@gmail.com>
2018-03-20 22:10:35 +0300

    Load all projects

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,9 +23,10 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+
+set :branch,          :production
 ## Defaults:
 # set :scm,           :git
-# set :branch,        :master
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
