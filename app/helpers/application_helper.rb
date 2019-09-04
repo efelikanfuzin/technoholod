c9ad64670b709c97760d738f412851e71dc8ac56
Alex Klimov <efelikanfuzin@gmail.com>
2019-09-04 04:16:06 +0300

    Немного поправил стили для продуктов

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def last_in_row(index)
     (index % 3).zero? ? 'product_item-last_in_row' : ''
   end
+
+  def selected_product_class(slug, product)
+    slug == product&.slug ? 'selected-product' : ''
+  end
 end
