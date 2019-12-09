33f8663055e5a45efa03a95b311ffa6c851ba04b
Alex Klimov <efelikanfuzin@gmail.com>
2019-12-09 23:44:20 +0300

    Update ckeditor_init.js

--- app/assets/javascripts/administrate/ckeditor_init.js
+++ app/assets/javascripts/administrate/ckeditor_init.js
@@ -1,3 +1,12 @@
 document.querySelectorAll('[data="ckeditor"]').forEach(field => {
-  CKEDITOR.replace(field, { "language": "ru" });
+  CKEDITOR.replace(field, { 
+    "language": "ru",
+    "filebrowserBrowseUrl": "/ckeditor/attachment_files",
+    "filebrowserFlashBrowseUrl": "/ckeditor/attachment_files",
+    "filebrowserFlashUploadUrl": "/ckeditor/attachment_files",
+    "filebrowserImageBrowseLinkUrl": "/ckeditor/pictures",
+    "filebrowserImageBrowseUrl": "/ckeditor/pictures",
+    "filebrowserImageUploadUrl": "/ckeditor/pictures",
+    "filebrowserUploadUrl": "/ckeditor/attachment_files",
+  });
 });
